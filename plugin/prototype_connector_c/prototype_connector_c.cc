e2e48efec1980b377a63e431a40c662e3b6aa31b
kahole <kristian@hole.priv.no>
2018-11-13 10:07:17 +0100

    removes superflous code

--- plugin/prototype_connector_c/prototype_connector_c.cc
+++ plugin/prototype_connector_c/prototype_connector_c.cc
@@ -39,36 +39,6 @@
 #include <cppconn/statement.h>
 #include <iostream>
 
-
-
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/param.h>
-#include <sys/socket.h>
-#include <sys/file.h>
-#include <sys/time.h>
-#include <netinet/in_systm.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <iostream>
-uint16_t in_cksum(uint16_t *addr, unsigned len);
-int ping(std::string);
-#define	DEFDATALEN	(64-ICMP_MINLEN)
-#define	MAXIPLEN	60
-#define	MAXICMPLEN	76
-#define	MAXPACKET	(65536 - 60 - ICMP_MINLEN)
-
-
-
-
 /* instrument the memory allocation */
 #ifdef HAVE_PSI_INTERFACE
 static PSI_memory_key key_memory_rewrite_example;
@@ -89,35 +59,15 @@ static int plugin_init(MYSQL_PLUGIN) {
 #define key_memory_rewrite_example PSI_NOT_INSTRUMENTED
 #endif /* HAVE_PSI_INTERFACE */
 
-// static int rewrite_lower(MYSQL_THD, mysql_event_class_t event_class,
-//                          const void *event) {
-//   if (event_class == MYSQL_AUDIT_PARSE_CLASS) {
-//     const struct mysql_event_parse *event_parse =
-//         static_cast<const struct mysql_event_parse *>(event);
-//     if (event_parse->event_subclass == MYSQL_AUDIT_PARSE_PREPARSE) {
-//       size_t query_length = event_parse->query.length;
-//       char *rewritten_query = static_cast<char *>(
-//           my_malloc(key_memory_rewrite_example, query_length + 1, MYF(0)));
-
-//       for (unsigned i = 0; i < query_length + 1; i++)
-//         rewritten_query[i] = tolower(event_parse->query.str[i]);
-
-//       event_parse->rewritten_query->str = rewritten_query;
-//       event_parse->rewritten_query->length = query_length;
-//       *((int *)event_parse->flags) |=
-//           (int)MYSQL_AUDIT_PARSE_REWRITE_PLUGIN_QUERY_REWRITTEN;
-//     }
-//   }
-
-//   return 0;
-// }
 
 static int connect_database(MYSQL_THD, mysql_event_class_t event_class,
                             const void *event) {
     if (event_class == MYSQL_AUDIT_PARSE_CLASS) {
         const struct mysql_event_parse *event_parse =
             static_cast<const struct mysql_event_parse *>(event);
+
         if (event_parse->event_subclass == MYSQL_AUDIT_PARSE_PREPARSE) {
+
             // try {
                 sql::Driver *driver;
                 sql::Connection *con;
@@ -126,10 +76,13 @@ static int connect_database(MYSQL_THD, mysql_event_class_t event_class,
 
                 /* Create a connection */
                 driver = get_driver_instance();
-                // con = driver->connect("tcp://atum20.no.oracle.com:13000", "hola", "bananpose");
-                con = driver->connect("tcp://127.0.0.1:12100", "root", "");
-                // con = driver->connect("tcp://atum26.no.oracle.com:12100", "heggen", "abc");
+                
                 /* Connect to the MySQL test database */
+                con = driver->connect("tcp://127.0.0.1:12100", "root", "");
+
+                //con = driver->connect("tcp://atum20.no.oracle.com:13000", "hola", "bananpose");
+				//con = driver->connect("unix:///export/home/tmp/kahole/mysql-server/bld2/mysql-test/var/tmp/mysqld.1.sock", "root", "");
+				
                 con->setSchema("test");
 
                 stmt = con->createStatement();
@@ -150,9 +103,7 @@ static int connect_database(MYSQL_THD, mysql_event_class_t event_class,
             //     std::cout << "test" << std::endl;
             // }
 
-            // Simple ping to test if plugin can do networking things.
-            // ping("127.0.0.1");
-
+			//rewrite to lowercase
 
             size_t query_length = event_parse->query.length;
             char *rewritten_query = static_cast<char *>(
@@ -202,177 +153,3 @@ mysql_declare_plugin(audit_log){
     NULL,               /* reserverd                     */
     0                   /* flags                         */
 } mysql_declare_plugin_end;
-
-
-
-
-
-
-
-
-int ping(std::string target)
-{
-
-    int s, i, cc, packlen, datalen = DEFDATALEN;
-	struct hostent *hp;
-	struct sockaddr_in to, from;
-	// struct ip *ip;
-	u_char *packet, outpack[MAXPACKET];
-	char hnamebuf[MAXHOSTNAMELEN];
-	std::string hostname;
-	struct icmp *icp;
-	int ret, fromlen, hlen;
-	fd_set rfds;
-	struct timeval tv;
-	int retval;
-	struct timeval start, end;
-	int end_t;
-	bool cont = true;
-
-	to.sin_family = AF_INET;
-
-	to.sin_addr.s_addr = inet_addr(target.c_str());
-	if (to.sin_addr.s_addr != (u_int)-1)
-		hostname = target;
-	else 
-	{
-		hp = gethostbyname(target.c_str());
-		if (!hp)
-		{
-			std::cerr << "unknown host "<< target << std::endl;
-			return -1;
-		}
-		to.sin_family = hp->h_addrtype;
-		bcopy(hp->h_addr, (caddr_t)&to.sin_addr, hp->h_length);
-		strncpy(hnamebuf, hp->h_name, sizeof(hnamebuf) - 1);
-		hostname = hnamebuf;
-	}
-	packlen = datalen + MAXIPLEN + MAXICMPLEN;
-	if ( (packet = (u_char *)malloc((u_int)packlen)) == NULL)
-	{
-		std::cerr << "malloc error\n";
-		return -1;
-	}
-
-
-	if ( (s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
-	{
-		return -1; /* Needs to run as superuser!! */
-	}
-
-	icp = (struct icmp *)outpack;
-	icp->icmp_type = ICMP_ECHO;
-	icp->icmp_code = 0;
-	icp->icmp_cksum = 0;
-	icp->icmp_seq = 12345;
-	icp->icmp_id = getpid();
-
-
-	cc = datalen + ICMP_MINLEN;
-	icp->icmp_cksum = in_cksum((unsigned short *)icp,cc);
-
-	gettimeofday(&start, NULL);
-
-	i = sendto(s, (char *)outpack, cc, 0, (struct sockaddr*)&to, (socklen_t)sizeof(struct sockaddr_in));
-	if (i < 0 || i != cc)
-	{
-		if (i < 0)
-			perror("sendto error");
-		std::cout << "wrote " << hostname << " " <<  cc << " chars, ret= " << i << std::endl;
-	}
-	
-	FD_ZERO(&rfds);
-	FD_SET(s, &rfds);
-	tv.tv_sec = 1;
-	tv.tv_usec = 0;
-
-	while(cont)
-	{
-		retval = select(s+1, &rfds, NULL, NULL, &tv);
-		if (retval == -1)
-		{
-			perror("select()");
-			return -1;
-		}
-		else if (retval)
-		{
-			fromlen = sizeof(sockaddr_in);
-			if ( (ret = recvfrom(s, (char *)packet, packlen, 0,(struct sockaddr *)&from, (socklen_t*)&fromlen)) < 0)
-			{
-				perror("recvfrom error");
-				return -1;
-			}
-
-			// Check the IP header
-			// ip = (struct ip *)((char*)packet); 
-			hlen = sizeof( struct ip ); 
-			if (ret < (hlen + ICMP_MINLEN)) 
-			{ 
-				std::cerr << "packet too short (" << ret  << " bytes) from " << hostname << std::endl;;
-				return -1; 
-			} 
-
-			// Now the ICMP part 
-			icp = (struct icmp *)(packet + hlen); 
-			if (icp->icmp_type == ICMP_ECHOREPLY)
-			{
-				if (icp->icmp_seq != 12345)
-				{
-					std::cout << "received sequence # " << icp->icmp_seq << std::endl;
-					continue;
-				}
-				if (icp->icmp_id != getpid())
-				{
-					std::cout << "received id " << icp->icmp_id << std::endl;
-					continue;
-				}
-				cont = false;
-			}
-			else
-			{
-				std::cout << "Recv: not an echo reply" << std::endl;
-				continue;
-			}
-	
-			gettimeofday(&end, NULL);
-			end_t = 1000000*(end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec);
-
-			if(end_t < 1)
-				end_t = 1;
-
-			std::cout << "Elapsed time = " << end_t << " usec" << std::endl;
-			return end_t;
-		}
-		else
-		{
-			std::cout << "No data within one seconds.\n";
-			return 0;
-		}
-	}
-	return 0;
-}
-
-uint16_t in_cksum(uint16_t *addr, unsigned len)
-{
-  uint16_t answer = 0;
-  /*
-   * Algorithm is simple, using a 32 bit accumulator (sum), add
-   * sequential 16 bit words to it, and at the end, fold back all the
-   * carry bits from the t   16 bits into the lower 16 bits.
-   */
-  uint32_t sum = 0;
-  while (len > 1)  {
-    sum += *addr++;
-    len -= 2;
-  }
-
-  if (len == 1) {
-    *(unsigned char *)&answer = *(unsigned char *)addr ;
-    sum += answer;
-  }
-
-  sum = (sum >> 16) + (sum & 0xffff);
-  sum += (sum >> 16);
-  answer = ~sum;
-  return answer;
-}
