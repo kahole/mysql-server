895c49435de0c6ea1672c1f53d543871b87841a0
kahole <kristian@hole.priv.no>
2019-02-21 21:57:18 +0100

    notes in distributed_query.h

--- plugin/lundgren/distributed_query.h
+++ plugin/lundgren/distributed_query.h
@@ -9,11 +9,19 @@ struct Partition_query {
     std::string sql_statement;
     std::string interim_table_name;
     Node node;
-    // Er dette godt nok for å dekke alle Plugin-to-MySQL strategiene? Dekker ikke semi-join med to partisjonerte tabeller!!
-    // TODO: Bruk prep-statements for å opprette tabeller osv?
-    // hva med "insert join column". Da må DQM vite at prep_statementen må fylles med intern-spørring data
-    std::vector<std::string> prep_statements;
+    // Usikker på om prep_statements blir nyttig
+    //std::vector<std::string> prep_statements;
 };
+
+
+// TODO:
+// Ved å bruke generell interim target får vi nok fleksibilitet til å gjøre alle rene SQL strategier:
+// e.g semi-join: select join-kolonnen lokalt og lagre den remotely på nodene. Flertall fordi samme resultat ofte må på flere noder
+// NB! for å bruke temp tables blir vi nødt til å holde connections i live, det er sikkert lurt uansett!
+struct Interim_target {
+    std::string interim_table_name;
+    std::vector<Node> nodes;
+}
 
 // TODO: bruk stages for å fikse multi-step strategier, slik som semi-join med 2 partisjonerte tabeller
 struct Stage {
