94a9afc63618a790632284a8f005b0875e7e6afe
kahole <kristian@hole.priv.no>
2019-02-21 13:58:57 +0100

    adds comment and fixes gitignore

--- plugin/lundgren/distributed_query_rewriter.h
+++ plugin/lundgren/distributed_query_rewriter.h
@@ -270,7 +270,7 @@ std::string random_string( size_t length )
     auto randchar = []() -> char
     {
         const char charset[] =
-        "0123456789"
+          //"0123456789"
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         "abcdefghijklmnopqrstuvwxyz";
         const size_t max_index = (sizeof(charset) - 1);
