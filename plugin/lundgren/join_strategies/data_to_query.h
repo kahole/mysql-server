25d3038ec62b899603be02682a1c51f2ec7e39f5
kahole <kristian@hole.priv.no>
2019-02-22 16:24:31 +0100

    started modules

---
+++ plugin/lundgren/join_strategies/data_to_query.h
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "plugin/lundgren/distributed_query.h"
+#include "plugin/lundgren/partitions/node.h"
+#include "plugin/lundgren/partitions/partition.h"
+
+#ifndef LUNDGREN_DATA_TO_QUERY
+#define LUNDGREN_DATA_TO_QUERY
+
+
+// Pure Distributed_query strategy, can be fed into DQM
+
+
+static Distributed_query *make_data_to_query_distributed_query(L_Tables * tables) {
+
+
+
+
+}
+
+
+
+#endif  // LUNDGREN_DATA_TO_QUERY
