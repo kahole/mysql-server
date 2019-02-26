65458c427943b1d114a0a6cc48194f55e0a2bad7
Håvard Ola Eggen <havard.ola.eggen@gmail.com>
2019-02-26 13:53:20 +0100

    Added function for parsing parameters sent with the distributed flag.

--- plugin/lundgren/helpers.h
+++ plugin/lundgren/helpers.h
@@ -1,4 +1,7 @@
 #include <string.h>
+#include <vector>
+#include <sstream>
+#include <iostream>
 
 #ifndef LUNDGREN_HELPERS
 #define LUNDGREN_HELPERS
@@ -17,6 +20,41 @@ std::string random_string(size_t length) {
   std::generate_n(str.begin(), length, randchar);
   return str;
 }
+
+// Split string by delimiter: https://thispointer.com/how-to-split-a-string-in-c/
+std::vector<std::string> split(std::string strToSplit, char delimeter)
+{
+    std::stringstream ss(strToSplit);
+    std::string item;
+    std::vector<std::string> splittedStrings;
+    while (std::getline(ss, item, delimeter))
+    {
+       splittedStrings.push_back(item);
+    }
+    return splittedStrings;
+}
+
+// Parses the parameters in the passed comment and creates a lookup table
+std::map<std::string, std::string> parse_query_comments(const char *query) {
+  char delimiter;
+  std::string query_string = std::string(query);
+  int pos_start = query_string.find("<")+1;
+  int pos_end = query_string.find(">") - pos_start;
+  query_string = query_string.substr(pos_start, pos_end);
+
+  delimiter = ',';
+  std::vector<std::string> comment_parameters = split(query_string, delimiter);
+
+  delimiter = '=';
+  std::map<std::string, std::string> comment_parameter_lookup_table;
+  for (auto const parameter : comment_parameters) {
+    int pos_delimiter = parameter.find(delimiter);
+    comment_parameter_lookup_table[parameter.substr(0,pos_delimiter)] = parameter.substr(pos_delimiter+1);
+  } 
+
+  return comment_parameter_lookup_table;
+}
+
 
 
 #endif  // LUNDGREN_HELPERS
