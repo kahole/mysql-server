0e49fddb8b8d0247adb9a963e79364cfc1541fd1
kahole <kristian@hole.priv.no>
2019-03-06 15:49:16 +0100

    adds bloom strategy to flag parser

--- plugin/lundgren/helpers.h
+++ plugin/lundgren/helpers.h
@@ -34,8 +34,8 @@ std::vector<std::string> split(std::string strToSplit, char delimeter)
     return splittedStrings;
 }
 
-enum JOIN_STRATEGY {DATA_TO_QUERY, SEMI};
-const std::map<std::string, JOIN_STRATEGY> join_strategy_string_to_enum = {{"data_to_query", DATA_TO_QUERY}, {"semi", SEMI}};
+enum JOIN_STRATEGY {DATA_TO_QUERY, SEMI, BLOOM};
+const std::map<std::string, JOIN_STRATEGY> join_strategy_string_to_enum = {{"data_to_query", DATA_TO_QUERY}, {"semi", SEMI}, {"bloom", BLOOM}};
 
 struct L_parsed_comment_args {
   JOIN_STRATEGY join_strategy;
