d497f6cd3bcb569657c0493f20d233c91df0ee04
kahole <kristian@hole.priv.no>
2019-03-13 11:07:28 +0100

    adds self-node singleton with dynamic port

--- plugin/lundgren/partitions/node.h
+++ plugin/lundgren/partitions/node.h
@@ -1,28 +1,60 @@
 #include <string.h>
+#include "plugin/lundgren/internal_query/internal_query_session.h"
+#include "plugin/lundgren/internal_query/sql_resultset.h"
 
 #ifndef LUNDGREN_NODE
 #define LUNDGREN_NODE
 
-struct Node
-{
-    std::string host;
-    uint port;
-    std::string database;
-    std::string user;
-    bool is_self = false;
+struct Node {
+  std::string host;
+  uint port;
+  std::string database;
+  std::string user;
+  bool is_self = false;
 
-    Node(bool is_self_in) : is_self(is_self_in) {
-        host = "127.0.0.1";
-        port = 13010;
-        database = "test";
-        user = "root";
-    }
+  Node(bool is_self_in) : is_self(is_self_in) {
+    host = "127.0.0.1";
+    port = 13010;
+    database = "test";
+    user = "root";
+  }
 
-    Node(char * host_in, uint port_in, char * database_in, char * user_in) : port(port_in) {
-        host = std::string(host_in);
-        database = std::string(database_in);
-        user = std::string(user_in);
+  Node(char *host_in, uint port_in, char *database_in, char *user_in)
+      : port(port_in) {
+    host = std::string(host_in);
+    database = std::string(database_in);
+    user = std::string(user_in);
+  }
+};
+
+class SelfNode {
+  static SelfNode *instance;
+  Node internal_node = Node(true);
+
+  SelfNode(uint port) {
+    internal_node = Node(true);
+    internal_node.port = port;
+  }
+
+ public:
+  static Node getNode() {
+    if (!instance) {
+      Internal_query_session *session = new Internal_query_session();
+
+      session->execute_resultless_query("USE test");
+      // sjekk portnummer
+      std::string port_number_query = "SELECT @@port;";
+      Sql_resultset *result = session->execute_query(port_number_query.c_str());
+      if (result->get_rows() == 0) {
+        return Node(true);
+      }
+      // sett portnummer
+      uint port_number = (uint)result->getLong(6) + 10;
+
+      instance = new SelfNode(port_number);
     }
+    return instance->internal_node;
+  }
 };
 
 #endif  // LUNDGREN_NODE
