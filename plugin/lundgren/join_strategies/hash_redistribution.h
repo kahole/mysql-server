6fcc3eb8b6fdb23bf91041013439f18571ba9516
Håvard Ola Eggen <havard.ola.eggen@gmail.com>
2019-03-15 15:47:10 +0100

    Initial push for hash_redist method.

---
+++ plugin/lundgren/join_strategies/hash_redistribution.h
@@ -0,0 +1,14 @@
+
+
+#ifndef LUNDGREN_HASH_REDIST_JOIN
+#define LUNDGREN_HASH_REDIST_JOIN
+
+static Distributed_query *make_hash_redist_join_distributed_query(L_Parser_info *parser_info, L_parsed_comment_args parsed_args);
+
+
+
+static Distributed_query *make_hash_redist_join_distributed_query(L_Parser_info *parser_info, L_parsed_comment_args parsed_args) {
+
+}
+
+#endif  // LUNDGREN_HASH_REDIST_JOIN
