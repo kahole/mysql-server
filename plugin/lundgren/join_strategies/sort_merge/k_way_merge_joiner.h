da6f365e222add2b6a31a975c02a5c6e912401ab
kahole <kristian@hole.priv.no>
2019-03-19 16:28:37 +0100

    adds binary heap instead of k children heap

--- plugin/lundgren/join_strategies/sort_merge/k_way_merge_joiner.h
+++ plugin/lundgren/join_strategies/sort_merge/k_way_merge_joiner.h
@@ -1,9 +1,11 @@
 #include <mysqlx/xdevapi.h>
 //#include <queue>
 #include <tuple>
+#include <algorithm>
 
 #ifndef LUNDGREN_K_WAY
 #define LUNDGREN_K_WAY
+
 
 class K_way_node {
 private:
@@ -30,6 +32,105 @@ public:
         return popped_row;
     }
 };
+
+//----------------
+
+class Bin_heap {
+
+private:
+    int column_index;
+    std::vector<K_way_node> nodes;
+
+private:
+    static int left(int i) {
+        return 2*i;
+    }
+
+    static int right(int i) {
+        return 2*i+1;
+    }
+
+    static int parent(int i) {
+        return i/2;
+    }
+
+    int at(int i) {
+        return (int) nodes[i - 1].peek()[column_index];
+    }
+
+    bool node_empty(int i) {
+        return nodes[i - 1].is_empty();
+    }
+
+    void swap(int t, int f) {
+        std::iter_swap(nodes.begin() + t-1, nodes.begin() + f-1);
+        // K_way_node t_node = nodes[t-1];
+        // nodes[t-1] = nodes[f-1];
+        // nodes[f-1] = t_node;
+    }
+
+    void heapify() {
+        int heap_size = nodes.size();
+
+        for (int i = heap_size / 2; i > 0; --i) {
+            sift_down(i);
+        }
+    }
+
+    void sift_down(int i) {
+        int heap_size = nodes.size();
+        int l = left(i);
+        int r = right(i);
+
+        int smallest;
+
+        if (l <= heap_size && (node_empty(i) || at(l) < at(i))) {
+            smallest = l;
+        } else {
+            smallest = i;
+        }
+
+        if (r <= heap_size && (node_empty(smallest) || at(r) < at(smallest))) {
+            smallest = r;
+        } else {
+            smallest = i;
+        }
+
+        if (smallest != i) {
+            swap(i, smallest);
+            sift_down(smallest);
+        }
+    }
+
+public:
+    Bin_heap() {}
+    Bin_heap(std::vector<mysqlx::SqlResult*> streams, int c_index) {
+        column_index = c_index;
+        for (auto &s : streams) {
+            nodes.push_back(K_way_node(s));
+        }
+        heapify();
+    }
+
+    bool has_next() {
+        return !nodes[0].is_empty();
+    }
+
+    mysqlx::Row pop() {
+        mysqlx::Row tmp = nodes[0].next();
+        sift_down(1);
+        return tmp;
+    }
+
+    mysqlx::Row peek() {
+        return nodes[0].peek();
+    }
+};
+
+
+
+
+//----------------------
 
 class K_way_heap {
 private:
@@ -83,8 +184,8 @@ public:
 class K_way_merge_joiner {
 
 private:
-    K_way_heap lhs_heap;
-    K_way_heap rhs_heap;
+    Bin_heap lhs_heap;
+    Bin_heap rhs_heap;
 
     std::vector<mysqlx::Row> lhs_buffer;
     std::vector<mysqlx::Row> rhs_buffer;
@@ -95,8 +196,8 @@ private:
 public:
     K_way_merge_joiner(std::vector<mysqlx::SqlResult*> lhs, std::vector<mysqlx::SqlResult*> rhs, int lhs_column_index, int rhs_column_index) {
 
-        lhs_heap = K_way_heap(lhs, lhs_column_index);
-        rhs_heap = K_way_heap(rhs, rhs_column_index);
+        lhs_heap = Bin_heap(lhs, lhs_column_index);
+        rhs_heap = Bin_heap(rhs, rhs_column_index);
 
         this->lhs_column_index = lhs_column_index;
         this->rhs_column_index = rhs_column_index;
